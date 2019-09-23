beeb92fc8cb2cb087339c8e3a9e318808cd38792
Petani Kode <contoh@petanikode.com>
2019-09-23 18:03:22 +0700

    hello word

---
+++ app/src/main/java/com/example/muhammadamin5170411284/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.muhammadamin5170411284
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
